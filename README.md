e91b921cb9a647d03ee26b00199476d8b711341c
Evgenii Ryshkov <eryshkov@gmail.com>
2019-01-28 19:58:15 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,23 @@
+#  Home work 2
+
+<div align = "center">
+<img src="/screens/hw01.png" width="100%">  
+</div>
+
+<p align="center">
+<img src="https://img.shields.io/badge/PHP-7.2-orange.svg" alt="PHP-7.2"/>
+<img src="https://img.shields.io/badge/licence-MIT-lightgray.svg" alt="Licence MIT"/>
+</p>
+
+## Ответы на вопросы домашнего задания
+* находятся в файле [README.pdf](/README.pdf)
+
+## Main functionality
+* uses `var_dump` & `echo`
+
+## Credits
+* thanks to **Albert Stepantsev** and to his [awesome school](https://pr-of-it.ru/courses/php-1.html)
+
+## License
+
+This project is licensed under the MIT License.
---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
+    <title>HomeWork 2</title>
+</head>
+<body>
+<p></p>
+<div class="container">
+    <a href="/03" class="btn btn-primary">Task 3</a>
+    <a href="/04" class="btn btn-primary">Task 4</a>
+    <a href="/05" class="btn btn-primary">Task 5</a>
+    <a href="/06" class="btn btn-primary">Task 6</a>
+</div>
+</body>
+</html>
